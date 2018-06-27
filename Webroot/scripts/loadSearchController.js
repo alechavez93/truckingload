7a88eba39a74e70337f052c99d1f7b8bee65f38b
Alejandro Chavez <alejandro_chavez@ultimatesoftware.com>
2018-06-27 15:43:03 -0400

    Added UI filters and apply filters button

--- Webroot/scripts/loadSearchController.js
+++ Webroot/scripts/loadSearchController.js
@@ -22,13 +22,41 @@ app.controller("loadSearchController", ["$scope", "$log", "searchService", "conf
         });
     };
 
+    $scope.brokers = [
+        {
+            name: "All Brokers",
+            checked: true
+        },
+        {
+            name: "Blah 1",
+            checked: false
+        },
+        {
+            name: "Blah 1",
+            checked: false
+        }
+    ];
+
     // Gets updated array of loads
     $scope.getLoads = function(){
         let searchId = $scope.currentSearch.searchCriteriaId;
         loadService.getLoads(searchId)
             .then((loads) => {
                 $scope.loads = loads;
-            })
+                $scope.brokers = [ { name: "All Brokers", checked: true } ];
+                let brokerNames = [...new Set(loads.map( load => load.brokerName ))];
+                brokerNames.forEach((name) => {
+                    $scope.brokers.push({
+                        name: name,
+                        checked: false
+                    });
+                });
+            });
+    };
+
+    // Applies filters for loads
+    $scope.applyFilters = function () {
+
     };
 
     // Gets updated array of searches
@@ -43,5 +71,59 @@ app.controller("loadSearchController", ["$scope", "$log", "searchService", "conf
 
     // Init
     getSearches();
-}]);
 
+    $scope.states = [
+        { name: "All States", code: "ALL", checked: true },
+        { name: "Alabama", code: "AL", checked: false },
+        { name: "Alaska", code: "AK", checked: false },
+        { name: "Arizona", code: "AZ", checked: false },
+        { name: "Arkansas", code: "AR", checked: false },
+        { name: "California", code: "CA", checked: false },
+        { name: "Colorado", code: "CO", checked: false },
+        { name: "Connecticut", code: "CT", checked: false },
+        { name: "Delaware", code: "DE", checked: false },
+        { name: "District of Columbia", code: "DC", checked: false },
+        { name: "Florida", code: "FL", checked: false },
+        { name: "Georgia", code: "GA", checked: false },
+        { name: "Hawaii", code: "HI", checked: false },
+        { name: "Idaho", code: "ID", checked: false },
+        { name: "Illinois", code: "IL", checked: false },
+        { name: "Indiana", code: "IN", checked: false },
+        { name: "Iowa", code: "IA", checked: false },
+        { name: "Kansas", code: "KS", checked: false },
+        { name: "Kentucky", code: "KY", checked: false },
+        { name: "Louisiana", code: "LA", checked: false },
+        { name: "Maine", code: "ME", checked: false },
+        { name: "Maryland", code: "MD", checked: false },
+        { name: "Massachusetts", code: "MA", checked: false },
+        { name: "Michigan", code: "MI", checked: false },
+        { name: "Minnesota", code: "MN", checked: false },
+        { name: "Mississippi", code: "MS", checked: false },
+        { name: "Missouri", code: "MO", checked: false },
+        { name: "Montana", code: "MT", checked: false },
+        { name: "Nebraska", code: "NE", checked: false },
+        { name: "Nevada", code: "NV", checked: false },
+        { name: "New Hampshire", code: "NH", checked: false },
+        { name: "New Jersey", code: "NJ", checked: false },
+        { name: "New Mexico", code: "NM", checked: false },
+        { name: "New York", code: "NY", checked: false },
+        { name: "North Carolina", code: "NC", checked: false },
+        { name: "North Dakota", code: "ND", checked: false },
+        { name: "Ohio", code: "OH", checked: false },
+        { name: "Oklahoma", code: "OK", checked: false },
+        { name: "Oregon", code: "OR", checked: false },
+        { name: "Pennsylvania", code: "PA", checked: false },
+        { name: "Rhode Island", code: "RI", checked: false },
+        { name: "South Carolina", code: "SC", checked: false },
+        { name: "South Dakota", code: "SD", checked: false },
+        { name: "Tennessee", code: "TN", checked: false },
+        { name: "Texas", code: "TX", checked: false },
+        { name: "Utah", code: "UT", checked: false },
+        { name: "Vermont", code: "VT", checked: false },
+        { name: "Virginia", code: "VA", checked: false },
+        { name: "Washington", code: "WA", checked: false },
+        { name: "West Virginia", code: "WV", checked: false },
+        { name: "Wisconsin", code: "WI", checked: false },
+        { name: "Wyoming", code: "WY", checked: false }
+    ];
+}]);
